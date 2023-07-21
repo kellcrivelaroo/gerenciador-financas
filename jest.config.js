6cab0ff9d67beb6347d5c18ec9b2c532b2ddafd4
kellcrivelaroo <kellcrivelaro92@gmail.com>
2023-07-21 18:38:22 -0300

    deploy: ativando SWC do next (e removendo babel)

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest')
+
+// Providing the path to your Next.js app which will enable loading next.config.js and .env files
+const createJestConfig = nextJest({ dir: './' })
+
+// Any custom config you want to pass to Jest
+const customJestConfig = {
   // pastas irgnoradas pelo jest
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   // mapeamento do "path alias" do Next 13
@@ -15,3 +22,6 @@ module.exports = {
   // ambiente de teste
   testEnvironment: 'jest-environment-jsdom',
 }
+
+// createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration, which is async
+module.exports = createJestConfig(customJestConfig)
